ce8fa7ad8527f15023f22e0526ae419db92a4268
Subu <subu@cliqz.com>
2017-09-18 17:36:36 +0200

    fix export bug

--- rskenlm/src/lib.rs
+++ rskenlm/src/lib.rs
@@ -2,19 +2,20 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-
+#![crate_type = "lib"]
 #![feature(libc)]
-#[cfg(test)]
 
 pub mod kenlm;
 mod bindings;
+
+#[cfg(test)]
 mod tests {
-    use super::*;
+    use kenlm::KenLM;
 
 
     #[test]
     fn lm_load() {
-        let kenlm_model = kenlm::KenLM::from_file("src/test.arpa").unwrap();
+        let kenlm_model = KenLM::from_file("src/test.arpa").unwrap();
         println!{"Score : {:?}", kenlm_model.perplexity("screening a little")};
     }
 }
